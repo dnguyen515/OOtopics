0d187ad427c703b96e7952e77881a7bcbaffb411
dnguyen515 <dnguyen@iastate.edu>
2014-09-26 22:55:19 -0500

    Update inherit02.py

--- inherit02.py
+++ inherit02.py
@@ -27,55 +27,73 @@ class Vehicle(object):
         self.sold_on = sold_on
 
     def sale_price(self):
-        """Return the sale price for this vehicle as a float amount."""
+        """Return the sale price for this vehicle as a float amount.
+        """
         if self.sold_on is not None:
             return 0.0  # Already sold
         return 5000.0 * self.wheels
 
     def purchase_price(self):
-        """Return the price for which we would pay to purchase the vehicle."""
+        """Return the price for which we would pay to purchase the vehicle.
+        """
         if self.sold_on is None:
             return 0.0  # Not yet sold
         return self.base_sale_price - (.10 * self.miles)
         
     def repair_cost(self):
-    """ set up local variables cost_of_repair, date_of_repair
-    ... make these variables of type list so that each time 
-    ... a repair occurs, the fact can be retained.
+        """ set up local variables cost_of_repair, date_of_repair
+        ... make these variables of type list so that each time 
+        ... a repair occurs, the fact can be retained.
+        """
+        cost_of_repair = []
+        date_of_repair = []
+        repairCost = raw_input("Enter cost of the repair: ")
+        cost_of_repair.append(repairCost)
+        date = raw_input("Enter repair date: ")
+        date_of_repair.append(date)
+        for x in cost_of_repair:
+            print x
+        for x in date_of_repair:
+            print x
     
-
     @abstractmethod
     def vehicle_type(self):
-        """"Return a string representing the type of vehicle this is."""
+        """"Return a string representing the type of vehicle this is.
+        """
         pass
         
 class Car(Vehicle):
-    """A car for sale by Jeffco Car Dealership."""
+    """A car for sale by Jeffco Car Dealership.
+    """
 
     base_sale_price = 8000
     wheels = 4
 
     def vehicle_type(self):
-        """"Return a string representing the type of vehicle this is."""
+        """"Return a string representing the type of vehicle this is.
+        """
         return 'car'
 
 class Truck(Vehicle):
-    """A truck for sale by Jeffco Car Dealership."""
+    """A truck for sale by Jeffco Car Dealership.
+    """
 
     base_sale_price = 10000
     wheels = 4
 
     def vehicle_type(self):
-        """"Return a string representing the type of vehicle this is."""
+        """"Return a string representing the type of vehicle this is.
+        """
         return 'truck'
         
 class Motorcycle(Vehicle):
-    """A motorcycle for sale by Jeffco Car Dealership."""
+    """A motorcycle for sale by Jeffco Car Dealership.
+    """
 
     base_sale_price = 4000
     wheels = 2
 
     def vehicle_type(self):
-        """"Return a string representing the type of vehicle this is."""
+        """"Return a string representing the type of vehicle this is.
+        """
         return 'motorcycle'
-        
